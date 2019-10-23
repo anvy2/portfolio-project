b67baaec6dfd553f1e4a3e931c68d9aae3b5cae2
Anshul <anshul2679@gmail.com>
2019-10-23 18:56:57 +0530

    Homepage added and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    Title = models.CharField(max_length = 150)
+    Publication_date = models.DateTimeField()
+    Description = models.TextField()
+    image = models.ImageField(upload_to = 'images/')

f14d47b3e54c416be42fbc3a243c911e7343d161
Anshul <anshul2679@gmail.com>
2019-10-25 04:17:20 +0530

    server

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 class Blog(models.Model):
     Title = models.CharField(max_length = 150)
-    Publication_date = models.DateTimeField()
+    Date = models.DateTimeField()
     Description = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.Title
+
+    def summary(self):
+        return self.Description[:100]
+
+    def time(self):
+        return self.Date.strftime('%b %e %Y')
